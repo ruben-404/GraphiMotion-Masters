b66426a9ae3cadd8f7e008d679358bcf88e20df5
HugoDortoka <137056639+HugoDortoka@users.noreply.github.com>
2023-10-11 14:21:23 +0200

    serveis

--- index.php
+++ index.php
@@ -105,19 +105,19 @@ include 'funciones.php';
                 </div>
             </div>
         </div>
-        <div class="nosaltres">
-            <div class="nosaltresImg">
-                    <img src="imgg/nosaltres.png" alt="foto usu">
+        <div class="serveis">
+            <div>
+                <div class="nosaltresTitle">
+                    Serveis
                 </div>
-                <div class="nosaltresText">
-                    <div class="nosaltresTitle">
-                        Sobre Nosaltres
-                    </div>
-                    <div class="nosaltresCaja">
-                        <a >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</a>
-                    </div>
+                <div class="nosaltresCaja">
+                    <a >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</a>
                 </div>
             </div>
+            <div class="serveisImg">
+                <img src="imgg/serveis.png" alt="foto usu">
+            </div>
+        </div>
         <div class="contacte">
             <p>Conctate</p>
         </div>
